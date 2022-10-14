ffc9ee9856e2f35365277d649bc03f6ff7307845
Willian Garcias <williangarcias10@gmail.com>
2022-10-14 16:57:20 -0300

    admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import Intervalos
+
+class IntervalosAdmin(admin.ModelAdmin):
+    list_disply = ['id', 'context', 'genre']
+
+admin.site.register(Intervalos, IntervalosAdmin)
