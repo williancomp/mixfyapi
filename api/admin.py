ac4a4d8f0caf841040c3735bd5a8f574d73b5ca6
Willian Garcias <williangarcias10@gmail.com>
2022-10-14 22:25:21 -0300

    usuarios models

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 # Register your models here.
-from .models import Intervalos
+from .models import Intervalos, Usuarios
 
 class IntervalosAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Intervalos._meta.fields]
 
+class UsuariosAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Usuarios._meta.fields]
+
 admin.site.register(Intervalos, IntervalosAdmin)
+admin.site.register(Usuarios, UsuariosAdmin)
